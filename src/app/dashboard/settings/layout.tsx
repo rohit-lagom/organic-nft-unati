5da06cb364288595c9e5f38dc3a6f371e61a21d7
rohit-lagom <rohit@lagomchain.com>
2025-07-11 13:11:25 +0530

    fix(Settings): fix layout of settings page

--- src/app/dashboard/settings/layout.tsx
+++ src/app/dashboard/settings/layout.tsx
@@ -4,9 +4,9 @@ import { ReactNode } from 'react';
 
 export default function SettingsLayout({ children }: { children: ReactNode }) {
   return (
-    <div className="flex min-h-screen bg-[#242424] text-white">
+    <div className="flex min-h-screen  text-white">
       <div className="flex-1 flex flex-col">
-        <main className="flex-1 p-6 md:p-8 overflow-auto">
+        <main className="flex-1 p-6 md:p-4 overflow-auto">
           {children}
         </main>
       </div>
