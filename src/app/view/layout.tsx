72c3a389f11234096c5ac4cdb6eb30c2a85024d8
rohit-lagom <rohit@lagomchain.com>
2025-07-18 11:33:32 +0530

    Feix(Layout): fix layoit html error

--- src/app/view/layout.tsx
+++ src/app/view/layout.tsx
@@ -4,18 +4,17 @@ import { Inter } from 'next/font/google';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
-  title: 'Certified Products | On-chain Organic Certificates',
-  description: 'Explore certified organic products pinned to IPFS for verification and transparency.',
+    title: 'Certified Products | On-chain Organic Certificates',
+    description: 'Explore certified organic products pinned to IPFS for verification and transparency.',
 };
 
 export default function ViewCertificatesLayout({ children }: { children: ReactNode }) {
-  return (
-    <html lang="en">
-      <body className={`${inter.className} bg-[#121212] text-white`}>
-   
-        <main>{children}</main>
+    return (
+        <section className={`${inter.className} bg-[#121212] text-white`}>
 
-      </body>
-    </html>
-  );
+            <main>{children}</main>
+        </section>
+
+
+    );
 }
